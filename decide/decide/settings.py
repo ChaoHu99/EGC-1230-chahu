6e59a788b2db16ae0b2b482a3d210df6bf2a6cd9
ChaoHu99 <chahu@alum.us.es>
2021-02-12 11:15:13 +0100

    heroku-travis

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -164,6 +164,9 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+BASEURL = 'https://egc1230chahu.herokuapp.com'
+APIS = {}
+
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
@@ -183,3 +186,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
